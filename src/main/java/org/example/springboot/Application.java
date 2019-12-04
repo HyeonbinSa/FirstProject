1ea727a238c481f708231b9d7e22e44262f4de3a
sahyeonbin <shb1833@gmail.com>
2019-12-04 21:12:38 +0900

    Chapter 2.2 Test Code 추가

---
+++ src/main/java/org/example/springboot/Application.java
@@ -0,0 +1,11 @@
+package org.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //스프링 부트의 자동 설정, 스프링 bean 읽기와 생성을 모두 자동으로 설정; 항상 최상단에 위치해야함.
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args); //해당 함수때문에 WAS를 실행
+    }
+}
