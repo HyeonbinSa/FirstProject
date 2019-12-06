c7c5e36e5f947030d83a53964a17b7da95181af1
sahyeonbin <shb1833@gmail.com>
2019-12-06 16:21:59 +0900

    Chapter 4 Complete Mustache를 이용한 화면 표시(글 등록, 수정, 삭제 기능 구현)

--- src/main/java/org/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/example/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,11 @@
 package org.example.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> { //Dao와 같은 DB Layer 접근자
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/org/example/springboot/service/posts/PostsService.java
+++ src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -3,6 +3,7 @@ package org.example.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.domain.posts.Posts;
 import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsListResponseDto;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
 import org.example.springboot.web.dto.PostsUpdateRequestDto;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.GenerationType;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -38,5 +41,19 @@ public class PostsService {
                 .orElseThrow(()-> new
                         IllegalIdentifierException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)//.map(posts -> new PostsListResponseDto(posts))와 같음.
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
